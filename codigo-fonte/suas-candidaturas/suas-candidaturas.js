09d04fbd64102986daeaeaccdbe3c24d97df547c
Aly Oliveira <108914238+1alyciaoliveira@users.noreply.github.com>
2024-05-12 19:20:03 -0600

    Update suas-candidaturas.js

--- codigo-fonte/suas-candidaturas/suas-candidaturas.js
+++ codigo-fonte/suas-candidaturas/suas-candidaturas.js
@@ -6,14 +6,58 @@ function abrirModal () {
     var modalCandidatura = new bootstrap.Modal(modal);
     modalCandidatura.show();
 }
+
+
+/** app */
+const cards = document.querySelectorAll('.card');
+const dropzones = document.querySelectorAll('.dropzone');
+
+/** os cards */
+cards.forEach(card => {
+    card.addEventListener('dragstart', dragstart);
+    card.addEventListener('drag', drag);
+    card.addEventListener('dragend', dragend)
+})
+
+function dragstart(){
+    this.classList.add('is-dragging')
+}
+
+function drag(){
+}
+
+function dragend(){
+    this.classList.remove('is-dragging')
+}
+
+/** onde os cards serão soltos */  
+
+dropzones.forEach( dropzone => {
+dropzone.addEventListener('dragenter', dragenter)
+dropzone.addEventListener('dragover', dragover)
+dropzone.addEventListener('dragleave', dragleave)
+dropzone.addEventListener('drop', drop)
+})
+
+function dragenter(){
+}
+
+function dragover(){
+    
+    this.classList.add('over')
+    const cardBeingDragged = document.querySelector('.is-dragging')
+    this.appendChild(cardBeingDragged)
+}
+function dragleave(){ 
+    this.classList.remove('over')
+}
+
+function drop(){   
+    this.classList.remove('over')
+}
+
 // Carregar os dados que já estao no local storage ao abrir
-window.onload = function() {
-    var db = JSON.parse(localStorage.getItem('db_vaga'));
- 
-    if (db && db.length > 0) {
-        exibeVagas(db);
-    }
-};
+
  
 // [CRUD] Estruturando e separando o CRUD em partes
  
@@ -47,7 +91,7 @@ function inserirVaga() {
     db.push(novaVaga);
    
     localStorage.setItem('db_vaga', JSON.stringify(db));
-    exibeVagas(db)
+    window.location.reload();
 }
 //READ
 function exibeVagas(db) {
@@ -59,13 +103,17 @@ function exibeVagas(db) {
  
         // adiciona info
         card.innerHTML = `
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                    <h5 class="card-title">${infoVaga.Cargo}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${infoVaga.Empresa}</h6>
-                    <p class="card-text">${infoVaga.Descricao}</p>
-                    <p class="card-text">${infoVaga.Localidade}</p>
-                    <p class="card-text">${infoVaga.Status}</p>
+            <div class="dropzone">
+                <div class="card" draggable="true">
+                    <div class="content">
+                        <div class="card-body">
+                            <h5 class="card-title">${infoVaga.Cargo}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">${infoVaga.Empresa}</h6>
+                            <p class="card-text">${infoVaga.Descricao}</p>
+                            <p class="card-text">${infoVaga.Localidade}</p>
+                            <p class="card-text">${infoVaga.Status}</p>
+                        </div>
+                    </div>
                 </div>
             </div>`;
  
@@ -73,49 +121,22 @@ function exibeVagas(db) {
         var sectionId;
         switch(infoVaga.Status) {
             case 'Aplicado':
-                sectionId = 'aplicadasCards';
+                sectionId = 'aplicadas';
                 break;
             case 'Em processo':
-                sectionId = 'emProcessoCards';
+                sectionId = 'emProcesso';
                 break;
             case 'Aprovado':
-                sectionId = 'aprovadoCards';
+                sectionId = 'aprovado';
                 break;
             case 'Rejeitado':
-                sectionId = 'rejeitadoCards';
+                sectionId = 'rejeitado';
                 break;
             default:
-                sectionId = 'aplicadasCards'; 
+                sectionId = 'aplicadas'; 
         }
  
         // Sobe a info ao card
         document.getElementById(sectionId).appendChild(card);
     });
 }
-//UPDATE
-/* function updateVaga(id, vaga) {
-
-    let index = db.vaga.map(obj => obj.id).indexOf(id);
-
-   
-    db.vaga[index].Cargo = cargo.value,
-    db.vaga[index].Empresa = empresa.value,
-    db.vaga[index].Descricao = descricao.value,
-    db.vaga[index].Localidade = localidade.value,
-    db.vaga[index].Status = status.value,
-
-    displayMessage("Vaga alterada com sucesso");
-
-    
-    localStorage.setItem('db_vaga', JSON.stringify(db));
-}
-//DELETE
-function deleteVaga(id) {    
-   
-    db.vaga = db.vaga.filter(function (element) { return element.id != id });
-
-    displayMessage("Vaga removida com sucesso");
-
-    // Atualiza os dados no Local Storage
-    localStorage.setItem('db_vaga', JSON.stringify(db));
-}*/
