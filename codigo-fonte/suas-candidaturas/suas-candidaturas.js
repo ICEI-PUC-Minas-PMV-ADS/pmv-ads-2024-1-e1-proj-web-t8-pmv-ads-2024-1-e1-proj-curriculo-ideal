c432c7fc72fd4bf7b5c91c420d37a8eb83768e26
Alexsander Gomes <Alexsandergc@outlook.com>
2024-05-15 19:12:33 -0300

    revisando funcao drag in drop

--- codigo-fonte/suas-candidaturas/suas-candidaturas.js
+++ codigo-fonte/suas-candidaturas/suas-candidaturas.js
@@ -1,66 +1,18 @@
 var modal = document.getElementById('modalCandidatura');
 
- 
-function abrirModal () {
- 
+
+function abrirModal() {
+
     var modalCandidatura = new bootstrap.Modal(modal);
     modalCandidatura.show();
 }
 
 
-/** app */
-const cards = document.querySelectorAll('.card');
-const dropzones = document.querySelectorAll('.dropzone');
-
-/** os cards */
-cards.forEach(card => {
-    card.addEventListener('dragstart', dragstart);
-    card.addEventListener('drag', drag);
-    card.addEventListener('dragend', dragend)
-})
-
-function dragstart(){
-    this.classList.add('is-dragging')
-}
-
-function drag(){
-}
-
-function dragend(){
-    this.classList.remove('is-dragging')
-}
-
-/** onde os cards serão soltos */  
-
-dropzones.forEach( dropzone => {
-dropzone.addEventListener('dragenter', dragenter)
-dropzone.addEventListener('dragover', dragover)
-dropzone.addEventListener('dragleave', dragleave)
-dropzone.addEventListener('drop', drop)
-})
-
-function dragenter(){
-}
-
-function dragover(){
-    
-    this.classList.add('over')
-    const cardBeingDragged = document.querySelector('.is-dragging')
-    this.appendChild(cardBeingDragged)
-}
-function dragleave(){ 
-    this.classList.remove('over')
-}
-
-function drop(){   
-    this.classList.remove('over')
-}
-
 // Carregar os dados que já estao no local storage ao abrir
 
- 
+
 // [CRUD] Estruturando e separando o CRUD em partes
- 
+
 //CREATE
 
 function inserirVaga() {
@@ -70,39 +22,40 @@ function inserirVaga() {
     var localidade = document.getElementById('Localidade');
     var status = document.getElementById('Status');
     var db = JSON.parse(localStorage.getItem('db_vaga'));
- 
-    if(db == null) {
-        localStorage.setItem('db_vaga','[]');
+
+    if (db == null) {
+        localStorage.setItem('db_vaga', '[]');
         db = [];
     }
     let novoId = 1;
     if (db.length != 0)
         novoId = db[db.length - 1].id + 1;
- 
+
     let novaVaga = {
         id: novoId,
         Cargo: cargo.value,
-        Empresa : empresa.value,
+        Empresa: empresa.value,
         Descricao: descricao.value,
         Localidade: localidade.value,
         Status: status.value,
     };
- 
+
     db.push(novaVaga);
-   
+
     localStorage.setItem('db_vaga', JSON.stringify(db));
     window.location.reload();
 }
 //READ
 function exibeVagas(db) {
     // Pra cada vaga cadastrada, gerar um card novo com a informacao do local storage
-    db.forEach(function(infoVaga) {
+    db.forEach(function (infoVaga) {
         // cria o card
         var card = document.createElement('div');
         card.className = 'row';
- 
+
         // adiciona info
         card.innerHTML = `
+        <div class="board">
             <div class="dropzone">
                 <div class="card" draggable="true">
                     <div class="content">
@@ -115,11 +68,12 @@ function exibeVagas(db) {
                         </div>
                     </div>
                 </div>
-            </div>`;
- 
+            </div>
+        </div>`;
+
         // lógica pra determinar o status
         var sectionId;
-        switch(infoVaga.Status) {
+        switch (infoVaga.Status) {
             case 'Aplicado':
                 sectionId = 'aplicadas';
                 break;
@@ -133,10 +87,58 @@ function exibeVagas(db) {
                 sectionId = 'rejeitado';
                 break;
             default:
-                sectionId = 'aplicadas'; 
+                sectionId = 'aplicadas';
         }
- 
+
         // Sobe a info ao card
         document.getElementById(sectionId).appendChild(card);
     });
+}
+
+/** app */
+const cards = document.querySelectorAll('.card');
+const dropzones = document.querySelectorAll('.dropzone');
+
+/** os cards */
+cards.forEach(card => {
+    card.addEventListener('dragstart', dragstart);
+    card.addEventListener('drag', drag);
+    card.addEventListener('dragend', dragend)
+})
+
+function dragstart() {
+    this.classList.add('is-dragging')
+}
+
+function drag() {
+}
+
+function dragend() {
+    this.classList.remove('is-dragging')
+}
+
+/** onde os cards serão soltos */
+
+dropzones.forEach(dropzone => {
+    dropzone.addEventListener('dragenter', dragenter)
+    dropzone.addEventListener('dragover', dragover)
+    dropzone.addEventListener('dragleave', dragleave)
+    dropzone.addEventListener('drop', drop)
+})
+
+function dragenter() {
+}
+
+function dragover() {
+
+    this.classList.add('over')
+    const cardBeingDragged = document.querySelector('.is-dragging')
+    this.appendChild(cardBeingDragged)
+}
+function dragleave() {
+    this.classList.remove('over')
+}
+
+function drop() {
+
 }
