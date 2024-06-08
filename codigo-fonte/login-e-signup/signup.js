02c95eb8bcf4a0cd43eed7976524dc9e269906f7
Alycia Oliveira <1alycia.oliveira@gmail.com>
2024-06-08 17:14:33 -0600

    ajuste nos arquivos de login e sign up para permitir o logout. Falta adicionar validacao para email duplicado

--- codigo-fonte/login-e-signup/signup.js
+++ codigo-fonte/login-e-signup/signup.js
@@ -1,61 +1,34 @@
-var db_vaga_inicial = {}
+function initPage() {
+    document.getElementById('btnSignUp').addEventListener('click', inserirCadastro);
+}
 
-// [CRUD] Estruturando e separando o CRUD em partes
-
-//CREATE
-var db = JSON.parse(localStorage.getItem('db_cadastro'));
-//if (!db) {
-  //  db = db_contatos_inicial
-//};
 function inserirCadastro() {
-    var nome = document.getElementById('nome');
-    var sobrenome = document.getElementById('sobrenome');
-    var email = document.getElementById('email');
-    var senha = document.getElementById('senha');
-    
-    var db = JSON.parse(localStorage.getItem('db_cadastro'));
+    var nome = document.getElementById('nome').value;
+    var sobrenome = document.getElementById('sobrenome').value;
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('senha').value;
 
-    if(db == null) {
-        localStorage.setItem('db_cadastro','[]');
-        db = [];
+    db_cadastro = JSON.parse(localStorage.getItem('db_cadastro')) || [];
+
+    let novoId = 1;
+    if (db_cadastro.length > 0) {
+        novoId = db_cadastro[db_cadastro.length - 1].id + 1;
     }
-    let novoId = 1
-    if (db.length != 0) 
-        novoId = db[db.length - 1].id + 1;
 
-    let novoCadastro = {
+    var novoCadastro = {
         id: novoId,
-        Nome: nome.value,
-        Sobrenome: sobrenome.value,
-        Email: email.value,
-        Senha: senha.value,
-    };
-
-    db.push(novoCadastro);
-    
-    localStorage.setItem('db_cadastro', JSON.stringify(db));
-
-    window.location.href= "../home/index.html";
-
-}
-
-const formularioLogin = document.getElementById("formulario");
-const btnLogin = document.getElementsByClassName("btn");
-function fazerLogin (){
-    let email= document.getElementById("email").value;
-    let senha= document.getElementById("senha").value;
-    var db = JSON.parse(localStorage.getItem('db_cadastro'));
-
-    if (db) {
-        var usuario = db.find(cadastro => cadastro.Email === email && cadastro.Senha === senha);
-        console.log (usuario)
-        if (usuario) {
-            window.location.href = "../home/index.html";
-        } else {
-            console.log ("invalido");
-            alert ("Usuário ou senha inválido");
-        }
-    } else {
-        console.log ("Não tem usuarios");
+        nome: nome,
+        sobrenome: sobrenome,
+        email: email,
+        senha: senha,
     }
+
+    db_cadastro.push(novoCadastro);
+    localStorage.setItem('db_cadastro', JSON.stringify(db_cadastro));
+
+    alert('Cadastro realizado com sucesso!');
+
+    window.location.href = "../home/index.html";
 }
+
+document.addEventListener('DOMContentLoaded', initPage);
